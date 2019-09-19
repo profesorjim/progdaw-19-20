785b5873be92f667c21a9bd0cb89ef855c37ca13
Prof. Juan Ant. Jimenez <profesorjim@gmail.com>
2019-09-19 11:02:34 +0200

    actualizacion

---
+++ Tema01/cuestionesejercicios.java
@@ -0,0 +1,26 @@
+public class cuestionesejercicios {
+  public static void main(String[]  args){
+    
+    System.out.printf("%-10s %-5s\n" , "Hola" , "Hello");
+    System.out.printf("%-10s %-5s\n" , "Rojo" , "Red");
+    System.out.printf("%-10s %-5s\n" , "Blanco" , "White");
+    System.out.printf("%-10s %-5s\n" , "Verde" , "Green");
+    System.out.printf("%-10s %-5s" , "Amarillo" , "Yellow\n");
+    System.out.printf("%-10s %-5s" , "Rosa" , "Pink\n");
+    System.out.printf("%-10s %-5s" , "Naranja" , "Orange\n");
+    System.out.printf("%-10s %-5s" , "Negro" , "Black\n");
+    System.out.printf("%-10s %-5s" , "Azul" , "Blue\n");
+    System.out.printf("%-10s %-5s" , "Coche" , "Car\n");
+    System.out.printf("%-10s %-5s" , "Coche" , "Car\n");
+    
+    System.out.println();
+    System.out.printf("%5s\n","*");
+    System.out.printf("%4s%2s\n","*","*");
+    System.out.printf("%3s%4s\n","*","*");
+    System.out.printf("%2s%6s\n","*","*");
+    System.out.printf("%9s\n","*********");
+    
+    
+    
+  }
+}
---
+++ Tema02/tema02.java
@@ -0,0 +1,28 @@
+public class tema02 {
+  public static void main (String[] args) {
+    float cantidad1=6.7f; 
+    float cantidad2=4.5f;
+    
+    int a=5,b=6;
+
+    cantidad1 = cantidad2*((float)a/b);
+
+    cantidad1 = 2.7f;
+    
+    System.out.println(a);
+    
+    a=a+3;
+    System.out.println(a);
+    b=a;
+    a=+3;
+    System.out.println(a);
+    a+=3;
+    System.out.println(a);
+    System.out.println(b);
+    
+    
+  
+  }
+
+}
+
