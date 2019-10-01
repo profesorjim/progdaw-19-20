24ee0f18a4dc9fa1cf25cf48f64d193c6db06e2e
Prof. Juan Ant. Jimenez <profesorjim@gmail.com>
2019-10-01 09:32:14 +0200

    actualizacion

---
+++ Tema04/EjemploPrecision.java
@@ -0,0 +1,24 @@
+public class EjemploPrecision {
+  public static void main (String[] args) {
+    long cantidad1;
+    long cantidad2;
+    long diferencia;
+    
+    double aux1;
+    double aux2;
+    
+    System.out.print("Intruduzca cantidad1 en €: ");
+    aux1 = Double.parseDouble(System.console().readLine());
+    cantidad1 = (long)(aux1 * 100);
+    System.out.print("Intruduzca cantidad2 en €: ");
+    aux2 = Double.parseDouble(System.console().readLine());
+    cantidad2 = (long)(aux2 * 100);
+    
+    diferencia = cantidad1 - cantidad2;
+    
+    System.out.println("La diferencia entre ambas cantidades es: "+diferencia/100.0);
+    
+    
+    
+  }
+}
